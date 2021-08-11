2b524ccc34125b80bea44478d5613b74a115b154
Qun Li <qun.li@zstack.io>
2021-08-11 18:56:24 +0800

    refactor DB schema
    Signed-off-by: Qun Li <qun.li@zstack.io>

--- conf/db/upgrade/V4.1.2.1__schema.sql
+++ conf/db/upgrade/V4.1.2.1__schema.sql
@@ -11,30 +11,44 @@ CREATE TABLE IF NOT EXISTS `zstack`.`CdpPolicyEO` (
     PRIMARY KEY  (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `zstack`.`CdpPolicyRefVO` (
-    `vmInstanceUuid` varchar(32) NOT NULL UNIQUE,
-    `policyUuid` varchar(32) NOT NULL,
-    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-    PRIMARY KEY  (`vmInstanceUuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 DROP VIEW IF EXISTS `zstack`.`CdpPolicyVO`;
 CREATE VIEW `zstack`.`CdpPolicyVO` AS SELECT uuid, name, description, retentionTimePerDay, recoveryPointPerSecond, state, lastOpDate, createDate FROM `zstack`.`CdpPolicyEO` WHERE deleted IS NULL;
 
 CREATE TABLE IF NOT EXISTS `zstack`.`CdpTaskVO` (
-    `vmInstanceUuid` varchar(32) NOT NULL UNIQUE,
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    `name` varchar(255) NOT NULL,
+    `description` varchar(2048) DEFAULT NULL,
+    `taskType` varchar(32) NOT NULL,
+    `policyUuid` varchar(32) NOT NULL,
     `backupStorageUuid` varchar(32) NOT NULL,
+    `backupBandwidth` bigint(20) unsigned NOT NULL,
+    `maxCapacity` bigint(20) unsigned NOT NULL,
+    `usedCapacity` bigint(20) unsigned NOT NULL,
+    `state` varchar(32) NOT NULL,
     `status` varchar(32) NOT NULL,
     `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
     `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-    PRIMARY KEY  (`vmInstanceUuid`)
+    PRIMARY KEY  (`uuid`),
+    INDEX `idxCdpTaskVOtaskType` (`taskType`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `zstack`.`CdpTaskVolumeRefVO` (
-    `volumeUuid` varchar(32) NOT NULL UNIQUE,
+CREATE TABLE IF NOT EXISTS `zstack`.`CdpTaskResourceRefVO` (
+    `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
+    `taskUuid` varchar(32) NOT NULL,
+    `resourceUuid` varchar(32) NOT NULL,
+    `resourceType` varchar(255) NOT NULL,
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY  (`id`),
+    INDEX `idxCdpTaskResourceRefVOtaskUuid` (`taskUuid`),
+    INDEX `idxCdpTaskResourceRefVOresourceUuid` (`resourceUuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `zstack`.`CdpVolumeHistoryVO` (
+    `volumeUuid` varchar(32) NOT NULL,
+    `backupStorageUuid` varchar(32) NOT NULL,
     `lastVolumePath` varchar(1024) DEFAULT NULL,
     `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
     `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-    PRIMARY KEY  (`volumeUuid`)
+    PRIMARY KEY  (`volumeUuid`, `backupStorageUuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
